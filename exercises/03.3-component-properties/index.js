48bfcebdc033d321c96aa4538e81407fe5625676
javi-mera <fcojmera@gmail.com>
2020-07-20 09:36:00 +0000

    finalizado

--- exercises/03.3-component-properties/index.js
+++ exercises/03.3-component-properties/index.js
@@ -6,11 +6,11 @@ const BootstrapCard = props => {
 	// 1) replace the hard-coded image, description, link, etc. With their property variable.
 	return (
 		<div className="card m-5">
-			<img className="card-img-top" src="https://ucarecdn.com/f8cf81eb-3bab-4bba-9431-668884eab174/-/resize/300x/" alt="Card image cap" />
+			<img className="card-img-top" src={props.imageUrl}"https://ucarecdn.com/f8cf81eb-3bab-4bba-9431-668884eab174/-/resize/300x/" alt="Card image cap" />
 			<div className="card-body">
 				<h5 className="card-title">{props.title}</h5>
-				<p className="card-text">Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer-songwriter.</p>
-				<a href="https://en.wikipedia.org/wiki/Bob_Dylan" className="btn btn-primary">
+				<p className="card-text">{props.description}Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer-songwriter.</p>
+				<a href={props.buttonUrl}"https://en.wikipedia.org/wiki/Bob_Dylan" className={props.buttonLabel}"btn btn-primary">
 					Go to wikipedia
 				</a>
 			</div>
@@ -18,7 +18,10 @@ const BootstrapCard = props => {
 	);
 };
 BootstrapCard.propTypes = {
-	title: PropType.string
+    title: PropType.string
+    imageUrl: 
+    description: 
+    
 		// 2) add here the new properties into the proptypes object
 };
 
--- exercises/03.5-alert-component/index.js
+++ exercises/03.5-alert-component/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import PropType from "prop-types";
 /**
  * here you have declare your Alert component and return
  * the html that bootstrap dictates for its alert component
  */
+const Alert = Props =>{
+    return (<div className="alert alert-danger" role="alert">
+  {Props.text}
+</div>)
+};
 
+Alert.PropType = {
+    text: PropType.string,
+}
 
 // here is where the alert component is being used, you don't have to edit this part,
 // but it helps you understan what properties is the component using
